0307f06e26c1cfe84239c4f8af3c7c5d4730e6a4
aldinoed <angga.antonov225@gmail.com>
2023-10-13 15:32:39 +0700

    Meresponsifkan background login dan form serta konten

--- resources/js/Pages/Auten.jsx
+++ resources/js/Pages/Auten.jsx
@@ -13,77 +13,85 @@ export default function login() {
             {/* body */}
 
             <div
-                className="container-fluid d-flex justify-content-center align-items-center"
+                className="container-fluid d-flex justify-content-center align-items-center "
                 style={{ height: "100vh" }}
             >
                 <div
-                    className="card rounded d-flex flex-row shadow bg-primary"
-                    style={{ border: "none", width: "65%", height: "70%" }}
+                    className="card rounded d-flex flex-row shadow rounded-4"
+                    style={{ border: "none", width: "50%", height: "65%" }}
                 >
-                    <div className="brand-login" style={{ width: "43%" }}>
-                        <img src="../../../storage/login1.png" alt="halo" />
-                        <div className="bg-white p-5" style={{ width: "57%" }}>
-                            <h5 className="fs-3 text-center"> Login </h5>
-                            <form
-                                method="POST"
-                                action="/login"
-                                className="mt-4 me-1"
-                            >
-                                <div className="mb-3">
-                                    <label
-                                        className="form-label"
-                                        forhtml="exampleInputEmail1"
-                                    >
-                                        Email/
-                                    </label>
-                                    <input
-                                        id="exampleInputEmail1"
-                                        className="form-control"
-                                        type="email"
-                                        aria-describedby="emailHelp"
-                                        name="email-login"
-                                    />
-                                </div>
-                                <div className="mb-3">
-                                    <label
-                                        className="form-label"
-                                        forhtml="exampleInputPassword1"
-                                    >
-                                        Password
-                                    </label>
-                                    <input
-                                        id="exampleInputPassword1"
-                                        className="form-control"
-                                        type="password"
-                                        name="password-login"
-                                    />
-                                </div>
-                                <div className="mb-3 form-check">
-                                    <input
-                                        id="exampleCheck1"
-                                        className="form-check-input"
-                                        type="checkbox"
-                                    />
-                                    <label
-                                        className="form-check-label"
-                                        forhtml="exampleCheck1"
-                                    >
-                                        Remember me
-                                    </label>
-                                </div>
+                    <div
+                        className="brand-login col-sm-4 rounded-start-4"
+                        style={{ width: "43%" }}
+                    >
+                        <h6>compnet</h6>
+                        <h2>Login</h2>
+                    </div>
+                    <div
+                        className="bg-white p-5 col-sm-6 rounded-4"
+                        style={{ width: "57%" }}
+                    >
+                        <form
+                            method="POST"
+                            action="/login"
+                            className="mt-4 me-1"
+                        >
+                            <div className="mb-3">
+                                <label
+                                    className="form-label"
+                                    forhtml="exampleInputEmail1"
+                                >
+                                    Username
+                                </label>
+                                <input
+                                    id="exampleInputEmail1"
+                                    className="form-control"
+                                    type="email"
+                                    aria-describedby="emailHelp"
+                                    name="email-login"
+                                />
+                            </div>
+                            <div className="mb-3">
+                                <label
+                                    className="form-label"
+                                    forhtml="exampleInputPassword1"
+                                >
+                                    Password
+                                </label>
+                                <input
+                                    id="exampleInputPassword1"
+                                    className="form-control"
+                                    type="password"
+                                    name="password-login"
+                                />
+                            </div>
+                            {/* <div className="mb-3 form-check">
+                                <input
+                                    id="exampleCheck1"
+                                    className="form-check-input"
+                                    type="checkbox"
+                                />
+                                <label
+                                    className="form-check-label"
+                                    forhtml="exampleCheck1"
+                                >
+                                    Remember me
+                                </label>
+                            </div> */}
+                            <div className="d-flex justify-content-center">
                                 <button
-                                    className="btn btn-primary"
+                                    className="btn btn-primary "
                                     type="submit"
                                 >
                                     Login
                                 </button>
-                                <br />
-                                <span>Tidak bisa login? Hubungi</span>
-                                <a href="https://api.whatsapp.com/send?phone=6283811051466">
-                                    Admin
-                                </a>
-                            </form>
-                        </div>
+                            </div>
+                            <br />
+                            <span>Tidak bisa login? Hubungi</span>
+                            <a href="https://api.whatsapp.com/send?phone=6283811051466">
+                                Admin
+                            </a>
+                        </form>
                     </div>
                 </div>
             </div>

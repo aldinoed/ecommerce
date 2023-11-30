43c405c15761d5ed0fb15d40feceddc8988208ac
aldinoed <angga.antonov225@gmail.com>
2023-11-30 07:39:13 +0700

    Dashboard admin tampil

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    //
+    public function userMan(){
+        return view("");
+    }
+    public function produkMan(){
+        return view("");
+    }
+    public function transactionMan(){
+        return view("");
+    }
+}
